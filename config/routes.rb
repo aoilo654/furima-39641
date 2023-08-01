8d450547f3979012be0150f348e87753e94980da
aoilo654 <aoooigme4201@gmail.com>
2023-08-01 18:50:08 +0900

    修正２

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
 
   root to: "items#index"
   resources :items, only: :index
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+  
 end
