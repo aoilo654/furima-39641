273ea405dc587738231468598f72e707cca809af
aoilo654 <aoooigme4201@gmail.com>
2023-08-01 17:07:08 +0900

    userモデル単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,17 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
 
+  
   validates :nickname, presence: true
   validates :email, presence: true
-  validates :encrypted_password, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  
+  validates :password, presence: true
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
     validates :last_name
     validates :first_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' } do
     validates :last_name_kana
     validates :first_name_kana
   end
