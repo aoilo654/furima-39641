1c49bf5ca4aa84cbce3a33c259c648b72a7c89a5
aoilo654 <aoooigme4201@gmail.com>
2023-08-01 19:17:21 +0900

    修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,8 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   
   validates :nickname, presence: true
-  validates :email, presence: true
   
-  validates :password, presence: true,
+  validates :password,
              format: { with: VALID_PASSWORD_REGEX,
              message: "is invalid. Include both letters and numbers"}
   
