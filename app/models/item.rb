5c1c605775339bbfecd8b94c1a88017d0bfa338e
aoilo654 <aoooigme4201@gmail.com>
2023-08-02 13:57:44 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_cost
+  belongs_to :sender
+  belongs_to :delivery_date
+  has_one_attached :image
+
+  belongs_to :user
 end
