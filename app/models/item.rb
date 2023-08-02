8bd43e1b5f81588f9520d079dc5f3f1a55b25211
aoilo654 <aoooigme4201@gmail.com>
2023-08-02 14:13:29 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,15 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   belongs_to :user
+
+  validates :item_name, :text, presence: true
+  validates :description, :text, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  validates :category_id, :text, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, :text, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_cost_id, :text, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sender_id, :text, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_date_id, :text, numericality: { other_than: 1 , message: "can't be blank"}
 end
