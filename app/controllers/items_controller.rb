edecef8c4870c0ca18fd7fe88514cf110b093dd7
aoilo654 <aoooigme4201@gmail.com>
2023-08-02 17:22:42 +0900

    商品登録機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,21 @@ class ItemsController < ApplicationController
     
   end
 
+  def new
+    @item = Item.new
+  end
   
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :delivery_cost_id, :sender_id, :delivery_date_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
