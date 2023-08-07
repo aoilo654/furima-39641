6b70b19714efe3c8c4f027a105c46a054337566b
aoilo654 <aoooigme4201@gmail.com>
2023-08-07 15:53:25 +0900

    addressバリデーション設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :order
 
+  validates :postcode,presence: true, format:{with: \A\d{3}[-]?\d{4}\z}
+  validates :city,presence: true
+  validates :block,presence: true
+
+  validates :phone_number,presence: true,numericality: {only_integer: true, message: "is invalid. Input only number" }
+  validates :phone_number,length: { minimum: 10, maximum: 11, message: "is too short" }
+
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
