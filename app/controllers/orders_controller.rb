28765d12f658a11c70f1c6009756463b6ab0626a
aoilo654 <aoooigme4201@gmail.com>
2023-08-07 20:13:27 +0900

    購入情報の登録機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,25 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   
   def index
+    @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id,item_id: :item_id)
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,18 +1,23 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postalcode, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
-    validates :postcode, format:{with: \A\d{3}[-]?\d{4}\z, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
+    validates :postcode, format:{with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
+    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :city
     validates :block
-
-    validates :phone_number,numericality: {only_integer: true, message: "is invalid. Input only number" }
-    validates :phone_number,length: { minimum: 10, maximum: 11, message: "is too short" }
   
     validates :user_id
     validates :item_id
+
   end
 
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :phone_number,presence: true,numericality: {only_integer: true, message: "is invalid. Input only number" }
+  validates :phone_number,length: { minimum: 10, maximum: 11, message: "is too short" }
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order.id)
+  end
 end
