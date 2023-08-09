871dcb9a0d518ec534ee8eb62356cc031cc999bf
aoilo654 <aoooigme4201@gmail.com>
2023-08-09 14:46:09 +0900

    決済処理を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,17 +14,18 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
-      render :index, status: :unprocessable_entity
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
+    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
@@ -35,5 +36,15 @@ class OrdersController < ApplicationController
     if Order.exists?(item_id: params[:item_id])
       redirect_to root_path
     end
+  end
+
+  def pay_item
+    binding.pry
+    Payjp.api_key = "sk_test_769649f7b3254dc56fbabbd7"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: set_item[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,12 +1,14 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
+    validates :token
     validates :postcode, format:{with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :city
     validates :block
+    
   
     validates :user_id
     validates :item_id
@@ -17,7 +19,7 @@ class OrderAddress
   validates :phone_number,length: { minimum: 10, maximum: 11, message: "is too short" }
 
   def save
-    order = Order.create(user_id: user_id, item_id: item_id)
+    order = Order.create(user_id: user_id, item_id: item_id, token: token )
     Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
